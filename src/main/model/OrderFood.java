8514970c3b0b2d952e940df8614ac81f89bd4abd
vaani27 <vaani27@student.ubc.ca>
2024-10-10 21:05:58 -0700

    Added specification for OrderFood

---
+++ src/main/model/OrderFood.java
@@ -0,0 +1,89 @@
+package model;
+import java.util.ArrayList;
+
+// Reprsents a order, with the item that has been ordered,
+// the total cost, the restaurant from which the foos is ordered,
+// and the delivery address; 
+
+public class OrderFood {
+
+    private ArrayList<Menu> items;      //items available to order from Menu
+    private double totalPrice;          //total price of food that is ordered
+    private Restaurant restaurant;      //the restraurant from which the food is ordered
+    private String deliveryAddress;     // the address at which the food is delivered
+
+    /*
+     * REQUIRES: the restaurant exists, atleast 1 item from menu is
+     *           selected and totalPrice > 0
+     * EFFECTS: the delivery Address of the order is set to deliveryAddress;
+     *          the totalPrice of the order is initially set to 0;
+     *          the restaurant in which the resevation is being made is set to restaurant.
+     */
+    public OrderFood() {
+       //STUB 
+    }
+
+    /* 
+     * REQUIRES: the item to exist in the restaurant
+     * MODIFIES: this
+     * EFFECTS: lets the customer add item to the order 
+     */
+    public void addItem(Menu item) {
+        //STUB
+    }
+
+     /* 
+     * REQUIRES: the item to exist in the restaurant and in the order
+     * MODIFIES: this
+     * EFFECTS: lets the customer remove item to the order 
+     */
+    public void removeItem(Menu item) {
+        //STUB
+    }
+
+
+     /* 
+     * MODIFIES: this
+     * EFFECTS: lets the view the items ordered
+     */
+    public void viewOrderDetails() {
+       //STUB
+    }
+    
+    
+
+    // Getters
+    public ArrayList<Menu> getItems() {
+        return null;
+    }
+
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    public Restaurant getRestaurant() {
+        return null;
+    }
+
+    public String getDeliveryAddress() {
+        return "";
+    }
+
+    // Setters
+    public void setItems(ArrayList<Menu> items) {
+        //STUB
+    }
+
+    public void setCustomer(Customer customer) {
+        //STUB
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+       //STUB
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        //STUB
+    }
+
+}
