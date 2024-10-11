84ad020c4316500383cf97bfa32f0cf158ae123a
vaani27 <vaani27@student.ubc.ca>
2024-10-11 11:03:02 -0700

    Added tests for Customer

---
+++ src/test/model/TestCustomer.java
@@ -0,0 +1,102 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCustomer {
+
+    private Customer customer;
+    private Restaurant r1; 
+    private MenuItems menuItem; 
+    private Review review; 
+    private Reservation reservation;
+
+    @BeforeEach
+    public void setUp() {
+        customer = new Customer("Katie", "Katie.Mill@example.com", "1234567890");
+        r1 = new Restaurant("Cactus club", "765 Broadway" , "Fusion");
+        menuItem = new MenuItems("Pizza", "cheese pizza", 9.99, "Main Course");
+        review = new Review(customer.getName(), "Amazing food", 5);
+        reservation = new Reservation(customer.getName(), "2024-09-15", "19:00", 3);
+    }
+    
+
+    @Test
+    public void testMakeReservation() {
+        customer.makeReservation(r1, "Katie", "2024-10-15", "19:00", 4);
+        assertEquals("Cactus club",r1.getRestaurantName());
+        assertEquals("765 Broadway",r1.getRestaurantLocation());
+        assertEquals("Fusion",r1.getCuisineType());
+        assertEquals("Katie", customer.getName());
+        assertEquals("2024-10-15",reservation.getReservationDate());
+        assertEquals("19:00",reservation.getReservationTime());
+        assertEquals(4,reservation.getNumberOfGuests());
+    }
+
+    @Test
+    public void testLeaveReview() {
+        customer.leaveReview(r1, 5, "Amazing food and great service!");
+        assertEquals("Cactus club",r1.getRestaurantName());
+        assertEquals("765 Broadway",r1.getRestaurantLocation());
+        assertEquals("Fusion",r1.getCuisineType());
+        assertEquals(5, review.getRating());
+        assertEquals("Amazing food and great service!", review.getReviewComment());
+    }
+
+    @Test
+    public void testPlaceOrder() {
+        ArrayList<MenuItems> items = new ArrayList<>();
+        items.add(menuItem);
+        customer.placeOrder(r1, items);
+        assertEquals("Cactus club",r1.getRestaurantName());
+        assertEquals("765 Broadway",r1.getRestaurantLocation());
+        assertEquals("Fusion",r1.getCuisineType());
+        assertEquals("Pizza",menuItem.getItemName());
+        assertEquals("cheese pizza",menuItem.getItemDescription());
+        assertEquals(9.99,menuItem.getItemPrice());
+        assertEquals("Main Course",menuItem.getItemCategory());
+
+    }
+
+    @Test
+    public void testViewRestaurants() {
+        customer.viewRestaurants();
+        assertEquals("Cactus club",r1.getRestaurantName());
+        assertEquals("765 Broadway",r1.getRestaurantLocation());
+        assertEquals("Fusion",r1.getCuisineType());
+   
+    }
+
+    @Test
+    public void testViewMenu() {
+        customer.viewMenu(r1);
+        assertEquals("Pizza",menuItem.getItemName());
+        assertEquals("cheese pizza",menuItem.getItemDescription());
+        assertEquals(9.99,menuItem.getItemPrice());
+        assertEquals("Main Course",menuItem.getItemCategory());
+ 
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals("Katie Mills", customer.getName());
+        assertEquals("Katie.Mills@gmail.com", customer.getEmail());
+        assertEquals("1234567890", customer.getPhoneNumber());
+    }
+
+    @Test
+    public void testSetters() {
+        customer.setName("Jude");
+        assertEquals("Jude", customer.getName());
+
+        customer.setEmail("jude@gmail.com");
+        assertEquals("jude@gmail.com", customer.getEmail());
+
+        customer.setPhoneNumber("0987654321");
+        assertEquals("0987654321", customer.getPhoneNumber());
+    }
+}
