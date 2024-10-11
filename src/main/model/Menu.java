6483a5cc794a58ad91be67762bdc0dcd44beb3fe
vaani27 <vaani27@student.ubc.ca>
2024-10-11 01:08:40 -0700

    Added implementation for Menu

--- src/main/model/Menu.java
+++ src/main/model/Menu.java
@@ -2,48 +2,54 @@ package model;
 
 import java.util.ArrayList;
 
-// Represents a Menu containing  a list menuItems
-class Menu {
-    private ArrayList<MenuItems> menuItems;  //contains all the menu items of a restaurant
+// Represents a Menu containing a list of menu items
+public class Menu {
+    private ArrayList<MenuItems> menuItems;  // Contains all the menu items of a restaurant
 
     /* 
-    * EFFECTS: creates an arraylist of menuItems
-    */
+     * EFFECTS: creates an empty arraylist of menuItems
+     */
     public Menu() {
-       //STUB
+        menuItems = new ArrayList<>();
     }
 
     /* 
      * MODIFIES: this
-     * EFFECTS: adds a the item to the menu
+     * EFFECTS: adds the item to the menu
      */
     public void addMenuItem(MenuItems item) {
-        //STUB
+        menuItems.add(item);
     }
 
     /* 
      * MODIFIES: this
-     * EFFECTS: removes the item from the menu of the restaurant
+     * EFFECTS: removes the item from the menu by name (if it exists)
      */
     public void removeMenuItem(String name) {
-        //STUB
+        menuItems.removeIf(item -> item.getItemName().equals(name));
     }
 
     /* 
-     * MODIFIES: this and Menu
-     * EFFECTS: updates the item in the menu of the restaurant
+     * MODIFIES: this
+     * EFFECTS: updates the menu item by name if it exists, changing its description and price
      */
     public void updateMenuItem(String name, String description, double price) {
-        //STUB
+        for (MenuItems item : menuItems) {
+            if (item.getItemName().equals(name)) {
+                item.setItemDescription(description);
+                item.setItemPrice(price);
+                break;
+            }
+        }
     }
 
     // Getters
     public ArrayList<MenuItems> getMenuItems() {
-        return null;
+        return menuItems;
     }
 
-    //Setters
+    // Setters
     public void setMenuItems(ArrayList<MenuItems> menuItems) {
-        //STUB
+        this.menuItems = menuItems;
     }
 }
