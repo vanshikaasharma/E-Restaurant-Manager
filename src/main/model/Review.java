18ff873a840da59986581f650c5a3be20608f181
vaani27 <vaani27@student.ubc.ca>
2024-10-11 01:26:25 -0700

    Added implementation of Review

--- src/main/model/Review.java
+++ src/main/model/Review.java
@@ -1,56 +1,57 @@
 package model;
 
 // Represents a review from a customer with the review 
-//comment, the rating of the restaurant and the name of the customer
+//comment, the rating of the restaurant, and the name of the customer
 public class Review {
-    private String reviewComment;       // the comment of their review
+    private String reviewComment;       // the comment of the review
     private int rating;                 // rating of the restaurant out of 5
-    private String customerName;        // the name customer giving the review
+    private String customerName;        // the name of the customer giving the review
 
     /*
-     * REQUIRES: reviewDescription has a non-zero length 
+     * REQUIRES: rating must be between 1 and 5 inclusive, reviewComment has a non-zero length 
      * EFFECTS: the rating of the restaurant is set to rating;
-     *          the comment in the review is given reviewComment;
-     *          the customer giving the review is given by customerName.
+     *          the comment in the review is set to reviewComment;
+     *          the customer giving the review is set to customerName.
      */
     public Review(String customerName, String reviewComment, int rating) {
-        //STUB
+        this.customerName = customerName;
+        this.reviewComment = reviewComment;
+        this.rating = rating;
+
     }
 
     /* 
      * MODIFIES: this
-     * EFFECTS: deletes the review given by the customer
+     * EFFECTS: deletes the review by clearing the comment and setting rating to 0
      */
-    public void deleteReview(){
-        //STUB
+    public void deleteReview() {
+        this.reviewComment = "";
+        this.rating = 0;
     }
-
 
     // Getters
     public String getReviewComment() {
-        return "";
+        return reviewComment;
     }
 
     public int getRating() {
-        return 0;
+        return rating;
     }
 
     public String getCustomerName() {
-        return "";
+        return customerName;
     }
 
     // Setters
-    public void setReviewComment(String reviewText) {
-        //STUB
+    public void setReviewComment(String reviewComment) {
+        this.reviewComment = reviewComment;
     }
 
     public void setRating(int rating) {
-        //STUB
+            this.rating = rating;
     }
 
-    public void setCustomerName(String customer) {
-        //STUB
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 }
-
-
