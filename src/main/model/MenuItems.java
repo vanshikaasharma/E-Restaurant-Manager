8b71e2b2798bec482a47fee90290cb3775f75017
vaani27 <vaani27@student.ubc.ca>
2024-10-11 01:17:01 -0700

    Added implementation for MenuItems

--- src/main/model/MenuItems.java
+++ src/main/model/MenuItems.java
@@ -4,59 +4,62 @@ package model;
 // the category of the item
 public class MenuItems {
 
-        private String itemName;            //name of the item
-        private String itemDescription;     // description of the item
-        private double itemPrice;           // the price of the item
-        private String itemCategory;        // the category in which the item belongs
-    
+    private String itemName;            // name of the item
+    private String itemDescription;     // description of the item
+    private double itemPrice;           // price of the item
+    private String itemCategory;        // category of the item (e.g., appetizer, main course, dessert)
 
-        /*
-        * REQUIRES: itemPrice > 0 and the item name has a non-zero length 
-        * EFFECTS: name of the item is set to itemName; description of 
-        *          the item is set to itemDescription; the item price is
-        *          given by itemPrice; the category in which the item belongs
-        *          is set to itemCategory.
-        */
-        public MenuItems(String itemName, String itemDescription, double itemPrice, String itemCategory) {
-           //STUB
-        }
-    
-        
-        // Getters
-        public String getItemName() {
-            return "";
-        }
-    
-        public String getItemDescription() {
-            return "";
-        }
-    
-        public double getItemPrice() {
-            return 0;
-        }
-    
-        public String getItemCategory() {
-            return "";
-        }
-    
-        // Setters
-        public void setItemName(String name) {
-            //STUB
-        }
-    
-        public void setItemDescription(String description) {
-            //STUB
-        }
-    
-        public void setItemPrice(double price) {
-            //STUB
-        }
-    
-        public void setItemCategory(String category) {
-            //STUB
-        }
+    /*
+    * REQUIRES: itemPrice > 0 and the item name has a non-zero length 
+    * EFFECTS: name of the item is set to itemName; description of 
+    *          the item is set to itemDescription; the item price is
+    *          given by itemPrice; the category in which the item belongs
+    *          is set to itemCategory.
+    */
+    public MenuItems(String itemName, String itemDescription, double itemPrice, String itemCategory) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemPrice = itemPrice;
+        this.itemCategory = itemCategory;
     }
-    
 
+    // Getters
+    public String getItemName() {
+        return itemName;
+    }
 
+    public String getItemDescription() {
+        return itemDescription;
+    }
 
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    // Setters
+    public void setItemName(String name) {
+        if (name.length() == 0) {
+            throw new IllegalArgumentException("Item name must be non-empty.");
+        }
+        this.itemName = name;
+    }
+
+    public void setItemDescription(String description) {
+        this.itemDescription = description;
+    }
+
+    public void setItemPrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Item price must be greater than 0.");
+        }
+        this.itemPrice = price;
+    }
+
+    public void setItemCategory(String category) {
+        this.itemCategory = category;
+    }
+}
