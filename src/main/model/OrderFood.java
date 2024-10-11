d6d5c426e2b6965ad292a3d081241593dbda90f0
vaani27 <vaani27@student.ubc.ca>
2024-10-10 22:21:40 -0700

    Made changes to specification of OrderFood Class

--- src/main/model/OrderFood.java
+++ src/main/model/OrderFood.java
@@ -1,15 +1,16 @@
 package model;
 import java.util.ArrayList;
 
-// Reprsents a order, with the item that has been ordered,
+// Reprsents a order, with the name of the customer, item that has been ordered,
 // the total cost, the restaurant from which the foos is ordered,
 // and the delivery address; 
 
 public class OrderFood {
 
-    private ArrayList<MenuItems> items;      //items available to order from Menu
+    private String customerName;               //name of the customer ordering food
+    private String restaurantName;             //name of the restaurant
+    private ArrayList<MenuItems> orderItems;      //items available to order from Menu
     private double totalPrice;          //total price of food that is ordered
-    private Restaurant restaurant;      //the restraurant from which the food is ordered
     private String deliveryAddress;     // the address at which the food is delivered
 
     /*
@@ -41,19 +42,11 @@ public class OrderFood {
         //STUB
     }
 
-
-     /* 
-     * MODIFIES: this
-     * EFFECTS: lets the view the items ordered
-     */
-    public void viewOrderDetails() {
-       //STUB
-    }
     
     
 
     // Getters
-    public ArrayList<MenuItems> getItems() {
+    public ArrayList<MenuItems> getOrderItems() {
         return null;
     }
 
@@ -61,8 +54,12 @@ public class OrderFood {
         return 0;
     }
 
-    public Restaurant getRestaurant() {
-        return null;
+    public String getRestaurantName() {
+        return "";
+    }
+
+    public String getCustomerName() {
+        return "";
     }
 
     public String getDeliveryAddress() {
@@ -70,17 +67,21 @@ public class OrderFood {
     }
 
     // Setters
-    public void setItems(ArrayList<MenuItems> items) {
+    public void setOrderItems(ArrayList<MenuItems> items) {
         //STUB
     }
 
-    public void setCustomer(Customer customer) {
-        //STUB
-    }
-
-    public void setRestaurant(Restaurant restaurant) {
+    public void setRestaurantName(String restaurantName) {
        //STUB
     }
+
+    public void setTotalPrice(int totalPrice) {
+        //STUB
+     }
+
+    public void setCustomerName(String CustomerName) {
+        //STUB
+     }
 
     public void setDeliveryAddress(String deliveryAddress) {
         //STUB
