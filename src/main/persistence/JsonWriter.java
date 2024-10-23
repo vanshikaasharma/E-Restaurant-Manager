b5a39e69454281680ea29ea66a61d5428227b999
vanshikaasharma <vanshika111.sharma@gmail.com>
2024-10-22 20:58:26 -0700

    Added specifications to JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,50 @@
 package persistence;
 
-public class JsonWriter {
+import model.Restaurant;
+import model.MenuItems;
+import model.Review;
+import model.Reservation;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.*;;
+
+// Represents a writer that writes JSON representation of restaurants to a file
+public class JsonWriter {
+    
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file
+    // cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        //STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of the restaurant to file
+    public void write(Restaurant restaurant) {
+        //STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        //STUB
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+       //STUB
+    }
+
+    // EFFECTS: converts restaurant to JSON object
+    private JSONObject restaurantToJson(Restaurant restaurant) {
+        return null;
+    }
 }
