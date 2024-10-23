f26626d3d8be6bf6ae1437dc2cf5b4dd1d5f5041
vanshikaasharma <vanshika111.sharma@gmail.com>
2024-10-22 23:54:58 -0700

    Added implementations to JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,51 +1,58 @@
 package persistence;
 
 import model.Restaurant;
-import model.MenuItems;
-import model.Review;
-import model.Reservation;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.*;
-import java.util.ArrayList;;
+import java.util.ArrayList;
 
-// Represents a writer that writes JSON representation of restaurants to a file
+// Represents a writer that writes JSON representation of restaurant data to file
 public class JsonWriter {
-    
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        
+        this.destination = destination;
     }
 
     // MODIFIES: this
-    // EFFECTS: opens writer; throws FileNotFoundException if destination file
-    // cannot
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        //STUB
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
-    // EFFECTS: writes JSON representation of the restaurant to file
+    // EFFECTS: writes JSON representation of list of restaurants to file
     public void write(ArrayList<Restaurant> restaurants) {
-        //STUB
+        JSONArray jsonArray = new JSONArray();
+
+        // Convert each restaurant to JSON and add to JSONArray
+        for (Restaurant restaurant : restaurants) {
+            //JSONObject jsonRestaurant = restaurant.toJson();
+            //jsonArray.put(jsonRestaurant);
+        }
+
+        // Create a main object to hold the list of restaurants
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("restaurants", jsonArray);
+
+        // Save the JSON data to file
+        saveToFile(jsonObject.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        //STUB
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-       //STUB
-    }
-
-    // EFFECTS: converts restaurant to JSON object
-    private JSONObject restaurantToJson(Restaurant restaurant) {
-        return null;
+        writer.print(json);
     }
 }
